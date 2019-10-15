684ad4b00e3e05342759f544abcee8387b0c1c73
jbehrend4 <47015844+jbehrend4@users.noreply.github.com>
2019-10-15 14:37:56 -0500

    Add files via upload

---
+++ Migrations/20191008234251_populateDiscount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Northwind.Migrations
+{
+    public partial class populateDiscount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "../../../", "Migrations", "Sql", "Populate_Discounts.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,18 @@
-﻿$(function(){
-    //alert("Hello World");
-});
+﻿$(function () {
+
+    var toastSoundEffect = new Audio('Media/toast.wav');
+
+    $('.code').on('click', function () {
+        toastSoundEffect.play();
+
+        $("#productName").html($(this).data('product'));
+        $('#discountCode').html($(this).data('code'));
+        $('#toast').toast('show');
+    });
+
+    $(document).on('keyup', function (e) {
+        if (e.key === 'Escape') {
+            $('.toast').toast('hide');
+        }
+    });
+})
